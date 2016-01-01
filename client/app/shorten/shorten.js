8723a2ad0e310aa6a97df298c7c05585a1125294
dakota-peel <koteman624@gmail.com>
2015-12-31 20:39:49 -0800

    #/signin works

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,9 +2,9 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
-    $scope.link = {};
+  $scope.link = {};
 
-    $scope.addLink = function () {
-      Links.addOne ($location);
-    }
-  });
+  $scope.addLink = function () {
+    Links.addOne ($location);
+  };
+});
