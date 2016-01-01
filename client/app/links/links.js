d1cad9e6b704250c8dc2872564eedf414ab1a5f7
dakota-peel <koteman624@gmail.com>
2015-12-31 17:31:57 -0800

    passing 14 tests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,12 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  // $scope.data = Links.getAll();
+  // {};
+  // console.log('testing');
+
+  $scope.data = {};
+  Links.getAll().then(function(data){
+      $scope.data.links = data;
+    });
 });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,7 +2,36 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
+  var getAll = function(data){
+    return $http({
+      method: 'GET',
+      url:'/api/links',
+      data: data
+    })
+    .then(function(resp){
+      console.log('testing',resp.data);
+      return resp.data;
+    });
+  }; 
+
+  var addOne = function(link){
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      data: link
+    })
+    .then(function (resp){
+      return resp;
+    })
+  };
+
+  return {
+    getAll: getAll,
+    addOne: addOne 
+  };
 })
+
+
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
   // it is responsible for authenticating our user
